25ef268e6898c3a28dab0b49d6341483333c1664
Fallsik <egor.ledovskoj.92@mail.ru>
2024-09-18 14:45:14 +0300

    Task3

---
+++ Labs_1st_smstr/3rd_Lab/Task3/Task3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main() {
+    setlocale(LC_ALL, "");
+
+    double a, b;
+    printf("¬ведите значение a: ");
+    scanf_s("%lf", &a);
+    printf("¬ведите значение b: ");
+    scanf_s("%lf", &b);
+
+    double multiplication = a * b;
+    double addition = a + b;
+    double subtraction = a - b;
+
+
+    printf("__________________________________________________________________\n");
+    printf("| %19s | %19s | %19s |\n", "a * b", "a + b", "a - b");
+    printf("__________________________________________________________________\n");
+    printf("| %4.2e * %-4.2e | %4.2e + %-4.2e | %4.2e - %-4.2e |\n", a, b, a, b, a, b);
+    printf("__________________________________________________________________\n");
+    printf("| %19.4e | %19.4e | %19.4e |\n", multiplication, addition, subtraction);
+    printf("__________________________________________________________________\n");
+
+    return 0;
+}
