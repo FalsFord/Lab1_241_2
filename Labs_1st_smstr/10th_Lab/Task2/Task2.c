fe1e00e1e8c3025e7a291c76dc008d366661a75a
Fallsik <egor.ledovskoj.92@mail.ru>
2024-10-25 20:40:45 +0300

    Commit

--- Labs_1st_smstr/10th_Lab/Task2/Task2.c
+++ Labs_1st_smstr/10th_Lab/Task2/Task2.c
@@ -1,33 +1,39 @@
-#include<stdio.h>
-#include<locale.h>
-#include<math.h>
+#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+#define M_PI 3.14
 int funcfact(int f) {
-	int factorial = 1;
-	for (int j = 1; j <= f; j++) factorial *= j;
-	return factorial;
+    int factorial = 1;
+    for (int j = 1; j <= f; j++) factorial *= j;
+    return factorial;
 }
 
 double sin_n(double x, int n) {
-	double sinx = 0;
-		for (int i = 1; i <= n; i++) {
-			if (i % 2 == 0)sinx -= pow(x, 2 * i - 1) / funcfact(2 * i - 1); else sinx += pow(x, 2 * i - 1) / funcfact(2 * i - 1);
-		}
-		return sinx;
+    double sinx = 0;
+    for (int i = 0; i < n; i++) {
+        int sign = (i % 2 == 0) ? 1 : -1;
+        sinx += sign * pow(x, 2 * i + 1) / funcfact(2 * i + 1);
+    }
+    return sinx;
 }
+
 int main() {
-	int x, n;
-	setlocale(LC_ALL, "Rus");
+    double x;
+    int n;
+    setlocale(LC_ALL, "Rus");
 
-	puts("Введите х ");
-	scanf_s("%d", &x);
+    puts("Введите х (в градусах) ");
+    scanf_s("%lf", &x);
 
-	puts("Введите n ");
-	scanf_s("%d", &n);
+    puts("Введите n ");
+    scanf_s("%d", &n);
 
-	printf("Синус си %f", sinf(x));
-	printf("\nSinx = %f", sin_n(x,n));
+    // Преобразование из градусов в радианы
+    x = x * M_PI / 180;
 
+    printf("Синус с использованием библиотеки: %f\n", sin(x));
+    printf("Синус с использованием разложения в ряд Тейлора: %lf\n", sin_n(x, n));
 
+    return 0;
 }
-
 
