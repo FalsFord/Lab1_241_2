4c553899336e57e50b5772039bad4339b60e6e25
Fallsik <egor.ledovskoj.92@mail.ru>
2024-10-27 16:05:55 +0300

    10_lab

--- Labs_1st_smstr/10th_Lab/Task2/Task2.c
+++ Labs_1st_smstr/10th_Lab/Task2/Task2.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <locale.h>
 #include <math.h>
-#define M_PI 3.14
+#define M_PI 3.1415926535897932384626
 int funcfact(int f) {
     int factorial = 1;
     for (int j = 1; j <= f; j++) factorial *= j;
@@ -16,33 +16,52 @@ double sin_n(double x, int n) {
     }
     return sinx;
 }
+
 double sin_eps(double x, double eps) {
     double sinx = 0;
-    for (int i = 0; i >=0; i++) {
+     for (int i = 0; fabs(sinx)<eps; i++) {
         int sign = (i % 2 == 0) ? 1 : -1;
         sinx += sign * pow(x, 2 * i + 1) / funcfact(2 * i + 1);
-        if (fabs(sinx) > eps)break;
     }
     return sinx;
 }
 
+double func_arccos(double x, int n) {
+    double arccos = (M_PI / 2) - x, mpl1=1, mpl2=1;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            mpl1 *= 2 * j - 1;
+            mpl2 *= 2 * j + 1;
+       }
+       arccos -= mpl1 * pow(x, mpl2) / mpl2;
+    }
+    return arccos;
+}
+
 int main() {
-    double x;
+    double x, eps;
     int n;
     setlocale(LC_ALL, "Rus");
 
-    puts("Введите х (в градусах) ");
+    puts("Введите х в радианах(x <= |6,28|)  ");
     scanf_s("%lf", &x);
 
     puts("Введите n ");
     scanf_s("%d", &n);
+    
+    getchar();
+    puts("Введите точность");
+    scanf_s("%lf", &eps);
 
+    if (x > 3.14)x =-1*(x - 3.14);
     // Преобразование из градусов в радианы
-    x = x * M_PI / 180;
+    /*x = x * M_PI / 180;*/
 
     printf("Синус с использованием библиотеки: %f\n", sin(x));
     printf("Синус с использованием разложения в ряд Тейлора: %lf\n", sin_n(x, n));
+    printf("Синус c точностью eps, ряд Маклорена: %lf\n", sin_eps(x, eps));
 
+    printf("Аркосинус с использованием разложения в ряд: %lf\n", func_arccos(x, n));
     return 0;
 }
 
