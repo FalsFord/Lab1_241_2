f45a04545f12241082cd631a9aac701e4ca44a57
Fallsik <egor.ledovskoj.92@mail.ru>
2024-10-30 22:47:25 +0300

    All

--- Labs_1st_smstr/10th_Lab/Task2/Task2.c
+++ Labs_1st_smstr/10th_Lab/Task2/Task2.c
@@ -11,7 +11,15 @@ int funcfact(int f) {
 double sin_n(double x, int n) {
     double sinx = 0;
     for (int i = 0; i < n; i++) {
-       (i % 2 == 0) ? 1 : -1;
+        sinx += (i % 2 == 0 ? 1 : -1) * pow(x, 2 * i + 1) / funcfact(2 * i + 1);
+    }
+    return sinx;
+}
+double sin_N(double x, int n) {
+    double sinx = 0;
+    printf("\n|   N   |    sin_N    |  отклонение |");
+    for (int i = 1; i < n+1; i++) {\
+        printf("\n|   %2d  |  %+.6f  | %+.5e|", i, sinx, sin(x) - sinx);
         sinx += (i % 2 == 0 ? 1 : -1) * pow(x, 2 * i + 1) / funcfact(2 * i + 1);
     }
     return sinx;
@@ -21,9 +29,9 @@ double sin_eps(double x, double eps) {
     double ans = 0;
     int i = 0;
     double term=0;
-    printf("\n|  N  |    sin_N    |  отклонение |");
+    /*printf("\n|  N  |    sin_N    |  отклонение |");*/
         do {
-            printf("\n|  %d  |  %+.6f  | %+.5e|", i, term, sin(x) - ans);
+            /*printf("\n|  %d  |  %+.6f  | %+.5e|", i, term, sin(x) - ans);*/
             term = (i % 2 == 0 ? 1 : -1) * pow(x, 2 * i + 1) / funcfact(2 * i + 1);
             ans += term;
             i++;
@@ -68,7 +76,7 @@ double func_arccos(double x, int n) {
 }
 
 int main() {
-    double x, eps, sum_eps;
+    double x, eps;
     int n;
     setlocale(LC_ALL, "Rus");
 
@@ -87,8 +95,9 @@ int main() {
 
     printf("Синус с использованием библиотеки: %f\n", sin(x));
     printf("Синус с использованием разложения в ряд Тейлора: %lf\n", sin_n(x, n));
+    printf("Синус через ряд Маклорена(ввод точности) равен %lf", sin_eps(x, eps));
 
-    sin_eps(x, eps);
+    sin_N(x, n);
 
     printf("\nАркосинус с использованием разложения в ряд: %lf\n", func_arccos(x, n));
     return 0;
