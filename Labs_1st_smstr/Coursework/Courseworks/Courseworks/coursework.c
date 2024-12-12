cd29a6aad69235e0bc2ad51ff07c294e3fec2502
Fallsik <egor.ledovskoj.92@mail.ru>
2024-12-12 13:06:46 +0300

    дддд

--- Labs_1st_smstr/Coursework/Courseworks/Courseworks/coursework.c
+++ Labs_1st_smstr/Coursework/Courseworks/Courseworks/coursework.c
@@ -1,8 +1,8 @@
-#include<stdio.h>
-#include<locale.h>
-#include<stdlib.h>
-#include<string.h>
-#include<ctype.h>
+#include <stdio.h>
+#include <locale.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 typedef struct Notebook {
     char fabricator[10];
@@ -13,15 +13,13 @@ typedef struct Notebook {
 } notebook_t;
 
 notebook_t* fill_notebooks(notebook_t* notebooks, int size) {
-    char* CPU;
     for (int i = 0; i < size; i++) {
-        CPU = notebooks[i].CPU;
         printf("Производитель ноутбука: ");
-        scanf("%s", notebooks[i].fabricator);
+        scanf("%9s", notebooks[i].fabricator);
         getchar();
 
         printf("Операционная система: ");
-        scanf("%s", notebooks[i].OS);
+        scanf("%9s", notebooks[i].OS);
         getchar();
 
         printf("Оперативная память(в ГБ): ");
@@ -29,11 +27,10 @@ notebook_t* fill_notebooks(notebook_t* notebooks, int size) {
         getchar();
 
         printf("Модель процессора: ");
-        CPU = gets(notebooks[i].CPU);
-
+        fgets(notebooks[i].CPU, sizeof(notebooks[i].CPU), stdin);
+        notebooks[i].CPU[strcspn(notebooks[i].CPU, "\n")] = 0; // Удаляем символ новой строки
 
         printf("Диагональ экрана(в дюймах): ");
-        fflush(stdin);
         scanf("%f", &notebooks[i].screenDiagonal);
         getchar();
         printf("\n");
@@ -41,12 +38,12 @@ notebook_t* fill_notebooks(notebook_t* notebooks, int size) {
     return notebooks;
 }
 
-void print_notebook(notebook_t* notebook) {
-    printf("Производитель ноутбука: %s\n", notebook->fabricator);
-    printf("Операционная система: %s\n", notebook->OS);
-    printf("Оперативная память(в ГБ): %d\n", notebook->RAM);
-    printf("Модель процессора: %s\n", notebook->CPU);
-    printf("Диагональ экрана(в дюймах): %f\n", notebook->screenDiagonal);
+void print_notebook(notebook_t notebook) {
+    printf("Производитель ноутбука: %s\n", notebook.fabricator);
+    printf("Операционная система: %s\n", notebook.OS);
+    printf("Оперативная память(в ГБ): %d\n", notebook.RAM);
+    printf("Модель процессора: %s\n", notebook.CPU);
+    printf("Диагональ экрана(в дюймах): %f\n", notebook.screenDiagonal);
     printf("\n");
 }
 
@@ -54,13 +51,13 @@ int* search_notebook(notebook_t* notebooks, int SIZE, char* CPU, int RAM) {
     int* countOfSeeked = malloc(SIZE * sizeof(int));
     for (int i = 0; i < SIZE; i++) {
         countOfSeeked[i] = -1;
-    };
-    if (CPU != '\0' && RAM != -1) {
+    }
+    if (CPU[0] != '\0' && RAM != -1) {
         for (int i = 0; i < SIZE; i++) {
             if (strcmp(notebooks[i].CPU, CPU) == 0 && notebooks[i].RAM == RAM) countOfSeeked[i] = i;
         }
     }
-    else if (CPU != '\0' && RAM == -1) {
+    else if (CPU[0] != '\0' && RAM == -1) {
         for (int i = 0; i < SIZE; i++) {
             if (strcmp(notebooks[i].CPU, CPU) == 0) countOfSeeked[i] = i;
         }
@@ -73,110 +70,85 @@ int* search_notebook(notebook_t* notebooks, int SIZE, char* CPU, int RAM) {
     return countOfSeeked;
 }
 
+int compare_fabricator(const void* a, const void* b) {
+    const notebook_t* notebook1 = (const notebook_t*)a;
+    const notebook_t* notebook2 = (const notebook_t*)b;
+    return strcmp(notebook1->fabricator, notebook2->fabricator);
+}
 
-void Sort(notebook_t* notebooks, int size, int n) {
-    if (n == 3) {
-        for (int i = 0; i < size - 1; i++) {
-            for (int j = 0; j < size - i - 1; j++) {
-                if (strcmp(notebooks[j].fabricator, notebooks[j + 1].fabricator) > 0) {
-                    notebook_t temp = notebooks[j];
-                    notebooks[j] = notebooks[j + 1];
-                    notebooks[j + 1] = temp;
-                }
-            }
-        }
-        for (int i = 0; i < size - 1; i++) {
-            for (int j = 0; j < size - i - 1; j++) {
-                if (strcmp(notebooks[j].OS, notebooks[j + 1].OS) > 0) {
-                    notebook_t temp = notebooks[j];
-                    notebooks[j] = notebooks[j + 1];
-                    notebooks[j + 1] = temp;
-                }
-            }
-        }
+int compare_OS(const void* a, const void* b) {
+    const notebook_t* notebook1 = (const notebook_t*)a;
+    const notebook_t* notebook2 = (const notebook_t*)b;
+    return strcmp(notebook1->OS, notebook2->OS);
+}
+
+notebook_t* sort_notebooks(notebook_t* notebooks, int size, int criteria) {
+    if (criteria == 1) {
+        qsort(notebooks, size, sizeof(notebook_t), compare_fabricator);
     }
-    else if (n == 1) {
-        for (int i = 0; i < size - 1; i++) {
-            for (int j = 0; j < size - i - 1; j++) {
-                if (strcmp(notebooks[j].fabricator, notebooks[j + 1].fabricator) > 0) {
-                    notebook_t temp = notebooks[j];
-                    notebooks[j] = notebooks[j + 1];
-                    notebooks[j + 1] = temp;
-                }
-            }
-        }
+    else if (criteria == 2) {
+        qsort(notebooks, size, sizeof(notebook_t), compare_OS);
+    }
+    else if (criteria == 3) {
+        qsort(notebooks, size, sizeof(notebook_t), compare_fabricator);
+        qsort(notebooks, size, sizeof(notebook_t), compare_OS);
     }
     else {
-        for (int i = 0; i < size - 1; i++) {
-            for (int j = 0; j < size - i - 1; j++) {
-                if (strcmp(notebooks[j].OS, notebooks[j + 1].OS) > 0) {
-                    notebook_t temp = notebooks[j];
-                    notebooks[j] = notebooks[j + 1];
-                    notebooks[j + 1] = temp;
-                }
-            }
+        printf("Ошибка: Неверный критерий сортировки\n");
+    }
+    return notebooks;
+}
+
+char* WriteFile(notebook_t* notebooks, int size) {
+    FILE* fp = fopen("Coursework.txt", "w");
+    if (fp == NULL) {
+        return "Ошибка открытия файла для записи";
+    }
+
+    for (int i = 0; i < size; i++) {
+        if (fprintf(fp, "Производитель ноутбука: %s\n", notebooks[i].fabricator) < 0 ||
+            fprintf(fp, "Операционная система: %s\n", notebooks[i].OS) < 0 ||
+            fprintf(fp, "Оперативная память(в ГБ): %d\n", notebooks[i].RAM) < 0 ||
+            fprintf(fp, "Модель процессора: %s\n", notebooks[i].CPU) < 0 ||
+            fprintf(fp, "Диагональ экрана(в дюймах): %f\n\n", notebooks[i].screenDiagonal) < 0) {
+            fclose(fp);
+            return "Ошибка записи данных в файл";
         }
     }
-}
 
-void WriteFile(notebook_t* notebooks, int size) {
-    FILE* fp;
-    fp = fopen("Coursework.txt", "w");
-    for (int i = 0; i < size; i++) {
-        fprintf(fp, "Производитель ноутбука: %s\n", notebooks[i].fabricator);
-        fprintf(fp, "Операционная система: %s\n", notebooks[i].OS);
-        fprintf(fp, "Оперативная память(в ГБ): %d\n", notebooks[i].RAM);
-        fprintf(fp, "Модель процессора: %s\n", notebooks[i].CPU);
-        fprintf(fp, "Диагональ экрана(в дюймах): %f\n", notebooks[i].screenDiagonal);
-        fprintf(fp, "\n");
-    }
     fclose(fp);
+    return NULL;
 }
 
-void ReadFile(notebook_t* notebooks, int size) {
+char* ReadFile(notebook_t* notebooks, int size) {
     FILE* fp = fopen("Coursework.txt", "r");
     if (fp == NULL) {
-        printf("Не удалось открыть файл\n");
-        return;
+        return "Не удалось открыть файл";
     }
 
+
     for (int i = 0; i < size; i++) {
-        // Считывание с пробелами в строках
-        if (fscanf(fp, "Производитель ноутбука: %[^\n]\n", notebooks[i].fabricator) != 1) {
-            printf("Ошибка при считывании производителя ноутбука для записи %d\n", i);
-            break;
+        if (fscanf(fp, "Производитель ноутбука: %9s\n", notebooks[i].fabricator) != 1 ||
+            fscanf(fp, "Операционная система: %9s\n", notebooks[i].OS) != 1 ||
+            fscanf(fp, "Оперативная память(в ГБ): %d\n", &notebooks[i].RAM) != 1 ||
+            fscanf(fp, "Модель процессора: %19[^\n]\n", notebooks[i].CPU) != 1 ||
+            fscanf(fp, "Диагональ экрана(в дюймах): %f\n\n", &notebooks[i].screenDiagonal) != 1) {
+            fclose(fp);
+            return "Ошибка при чтении данных из файла";
         }
-        if (fscanf(fp, "Операционная система: %[^\n]\n", notebooks[i].OS) != 1) {
-            printf("Ошибка при считывании операционной системы для записи %d\n", i);
-            break;
-        }
-        if (fscanf(fp, "Оперативная память(в ГБ): %d\n", &notebooks[i].RAM) != 1) {
-            printf("Ошибка при считывании оперативной памяти для записи %d\n", i);
-            break;
-        }
-        if (fscanf(fp, "Модель процессора: %[^\n]\n", notebooks[i].CPU) != 1) {
-            printf("Ошибка при считывании процессора для записи %d\n", i);
-            break;
-        }
-        if (fscanf(fp, "Диагональ экрана(в дюймах): %f\n", &notebooks[i].screenDiagonal) != 1) {
-            printf("Ошибка при считывании диагонали экрана для записи %d\n", i);
-            break;
-        }
-        fscanf(fp, "%*[\n]");
     }
 
     fclose(fp);
+    return NULL;
 }
 
-void add_notebook(notebook_t* notebooks, int num) {
-    char* CPU;
-    CPU = notebooks[num].CPU;
+notebook_t* AddChangeNotes(notebook_t* notebooks, int num) {
     printf("Производитель ноутбука: ");
-    scanf("%s", notebooks[num].fabricator);
+    scanf("%9s", notebooks[num].fabricator);
     getchar();
 
     printf("Операционная система: ");
-    scanf("%s", notebooks[num].OS);
+    scanf("%9s", notebooks[num].OS);
     getchar();
 
     printf("Оперативная память(в ГБ): ");
@@ -184,29 +156,57 @@ void add_notebook(notebook_t* notebooks, int num) {
     getchar();
 
     printf("Модель процессора: ");
-    CPU = gets(notebooks[num].CPU);
-
+    fgets(notebooks[num].CPU, sizeof(notebooks[num].CPU), stdin);
+    notebooks[num].CPU[strcspn(notebooks[num].CPU, "\n")] = 0;
 
     printf("Диагональ экрана(в дюймах): ");
-    fflush(stdin);
     scanf("%f", &notebooks[num].screenDiagonal);
     getchar();
     printf("\n");
+    return notebooks;
+}
+
+notebook_t* DeleteNotes(notebook_t* notebooks, int num, int SIZE)
+{
+    int n = num - 1;
+    int size = SIZE - 1;
+    notebook_t* temp = malloc(size * sizeof(notebook_t));
+    if (temp == NULL) {
+        printf("Ошибка выделения памяти\n");
+        return;
+    }
+    for (int i = 0; i < SIZE; i++) {
+        if (i < n) {
+            temp[i] = notebooks[i];
+        }
+        else if (i > n) {
+            temp[i - 1] = notebooks[i];
+        }
+    }
+    notebooks = realloc(notebooks, size * sizeof(notebook_t));
+    for (int i = 0; i < size; i++) {
+        notebooks[i] = temp[i];
+    }
+    return notebooks;
 }
 
 void main() {
     setlocale(LC_ALL, "Rus");
     int SIZE = 0;
-    int* countOfSeeked;
+
     printf("Введити количество записей: ");
-    scanf_s("%d", &SIZE);
-    notebook_t* notebooks;
-    notebooks =  malloc(SIZE*sizeof(notebook_t));
+    scanf("%d", &SIZE);
+    int* countOfSeeked = malloc(SIZE * sizeof(int));
+    notebook_t* notebooks = malloc(SIZE * sizeof(notebook_t));
+    notebook_t* temp;
     int num = 0;
-    char* searchProcUk;
+    char searchProc[20];
+    char* error = NULL;
     int a = 1;
     while (a) {
-        
+        ReadFile(notebooks, SIZE);
+
+
         printf("**********************************************\n");
         printf("*                     Menu                   *\n");
         printf("**********************************************\n");
@@ -217,17 +217,28 @@ void main() {
         printf("* 5. Завершить работу                        *\n");
         printf("**********************************************\n");
         scanf("%d", &num);
-
         switch (num) {
         case 1:
             num = 0;
             fill_notebooks(notebooks, SIZE);
-            WriteFile(notebooks, SIZE);
+            error = WriteFile(notebooks, SIZE);
+            if (error) {
+                printf("Ошибка: %s\n", error);
+            }
+            else {
+                printf("Файл успешно записан\n");
+            }
             break;
         case 2:
             num = 0;
-            ReadFile(notebooks, SIZE);
-            char searchProc[20];
+            error = WriteFile(notebooks, SIZE);
+            if (error) {
+                printf("Ошибка: %s\n", error);
+            }
+            else {
+                printf("Запись удалена и файл обновлён\n");
+            }
+            break;
             int searchRAM;
             printf("**********************************************\n");
             printf("*                    Поиск по                *\n");
@@ -235,13 +246,15 @@ void main() {
             printf("* 1. Процессору.                             *\n");
             printf("* 2. Оперативной памяти.                     *\n");
             printf("* 3. Процессору и оперативной памяти.        *\n");
+            printf("* 4. Вернуться в меню.                       *\n");
             printf("**********************************************\n");
             scanf("%d", &num);
             switch (num) {
             case 1:
                 printf("Введите название процессора: ");
                 getchar();
-                searchProcUk = gets(searchProc);
+                fgets(searchProc, sizeof(searchProc), stdin);
+                searchProc[strcspn(searchProc, "\n")] = 0; // Удаляем символ новой строки
                 searchRAM = -1;
                 break;
             case 2:
@@ -252,14 +265,17 @@ void main() {
             case 3:
                 getchar();
                 printf("Введите название процессора: ");
-                searchProcUk = gets(searchProc);
+                fgets(searchProc, sizeof(searchProc), stdin);
+                searchProc[strcspn(searchProc, "\n")] = 0; // Удаляем символ новой строки
                 printf("Введите объем оперативной памяти: ");
                 scanf("%d", &searchRAM);
+                break;
+            case 4:
                 break;
             }
             countOfSeeked = search_notebook(notebooks, SIZE, searchProc, searchRAM);
             for (int i = 0; i < SIZE; i++) {
-                if (countOfSeeked[i] != -1)print_notebook(&notebooks[i]);
+                if (countOfSeeked[i] != -1) print_notebook(notebooks[i]);
             }
             break;
         case 3:
@@ -271,19 +287,99 @@ void main() {
             printf("* 2. Изменить записи.                        *\n");
             printf("* 3. Удалить записи.                         *\n");
             printf("* 4. Чтение файла.                           *\n");
+            printf("* 5. Вернуться в меню.                       *\n");
             printf("**********************************************\n");
             scanf("%d", &num);
             switch (num) {
             case 1:
                 SIZE += 1;
-                notebooks = realloc(notebooks,SIZE * sizeof(notebook_t));
-                add_notebook(notebooks, SIZE);
-                WriteFile(notebooks, SIZE);
+                temp = realloc(notebooks, SIZE * sizeof(notebook_t));
+                if (temp == NULL) {
+                    printf("Ошибка выделения памяти\n");
+                    continue;
+
+                }
+                notebooks = temp;
+                AddChangeNotes(notebooks, SIZE - 1);
+                error = WriteFile(notebooks, SIZE);
+                if (error) {
+                    printf("Ошибка: %s\n", error);
+                }
+                else {
+                    printf("Запись добавлена и файл обновлён\n");
+                }
                 break;
-                // Добавьте обработку других случаев здесь
+            case 2:
+                num = 0;
+                printf("Введите номер записи(%d) для её изменения:", SIZE);
+                scanf("%d", &num);
+                AddChangeNotes(notebooks, num - 1);
+                error = WriteFile(notebooks, SIZE);
+                if (error) {
+                    printf("Ошибка: %s\n", error);
+                }
+                else {
+                    printf("Запись обновлена и файл обновлён\n");
+                }
+                break;
+            case 3:
+                num = 0;
+                printf("Введите номер записи(%d) для её удаления:", SIZE);
+                scanf("%d", &num);
+                DeleteNotes(notebooks, num, SIZE);
+                SIZE -= 1;
+                error = WriteFile(notebooks, SIZE);
+                if (error) {
+                    printf("Ошибка: %s\n", error);
+                }
+                else {
+                    printf("Запись удалена и файл обновлён\n");
+                }
+                break;
+            case 4:
+                error = ReadFile(notebooks, SIZE);
+                if (error) {
+                    printf("Ошибка: %s\n", error);
+                }
+                else {
+                    printf("Файл успешно прочитан\n");
+                    printf("\n");
+
+                }
+                for (int i = 0; i < SIZE; i++) print_notebook(notebooks[i]);
+                break;
+            case 5:
+                break;
             }
             break;
         case 4:
+            num = 0;
+            printf("**********************************************\n");
+            printf("*             Сортировать по                 *\n");
+            printf("**********************************************\n");
+            printf("* 1. Производителю.                          *\n");
+            printf("* 2. Операционной системе.                   *\n");
+            printf("* 3. Производителю и операционной системе.   *\n");
+            printf("* 4. Вернуться в меню.                       *\n");
+            printf("**********************************************\n");
+            scanf("%d", &num);
+            ReadFile(notebooks, SIZE);
+            switch (num) {
+            case 1:
+                sort_notebooks(notebooks, SIZE, num);
+                WriteFile(notebooks, SIZE);
+                break;
+            case 2:
+                sort_notebooks(notebooks, SIZE, num);
+                WriteFile(notebooks, SIZE);
+                break;
+            case 3:
+                sort_notebooks(notebooks, SIZE, num);
+                WriteFile(notebooks, SIZE);
+                break;
+            case 4:
+                break;
+            }
 
             break;
         case 5:
